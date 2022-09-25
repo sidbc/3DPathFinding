c16b6931fc74ea3793cca6ecf9cdbd1f11faaab4
Sid Bachtiar <sid.bachtiar@gmail.com>
2022-09-26 11:40:51 +1300

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+export var speed = 10
+onready var camera = $"../Camera"
+onready var navigation = $"../Navigation"
+var space_state : PhysicsDirectSpaceState
+var path = []
+var path_index = 0
+
+func _ready():
+	space_state = get_world().direct_space_state
+	
+func _input (event):
+	var mouse_event = event as InputEventMouseButton
+	if mouse_event == null:
+		return
+	if mouse_event.pressed and mouse_event.button_index == BUTTON_LEFT:
+		var from = camera.project_ray_origin(mouse_event.position)
+		# 1000 = length of ray
+		var to = from + camera.project_ray_normal(mouse_event.position) * 1000
+		var result = space_state.intersect_ray(from, to)
+		if result.collider:
+			path = navigation.get_simple_path(global_transform.origin, result.position)
+			path_index = 0
+			
+func _physics_process(delta):
+	if path_index < path.size():
+		var direction = path[path_index] - global_transform.origin
+		if direction.length() < 1:
+			path_index += 1
+		else:
+			var pos = direction.normalized()
+			look_at(pos * 1000, Vector3(0, 1, 0))
+			move_and_slide(pos * speed)
